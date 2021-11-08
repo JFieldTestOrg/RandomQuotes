e49d48254fa36f4b582e79e973bc6f7fa3f8aca6
jfield <jfield@TeamCity>
2021-11-08 14:44:14 +0100

    TeamCity change in 'DevOps / RandomQuotes' project: 'Build' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -78,6 +78,7 @@ object Build : BuildType({
     steps {
         exec {
             name = "GItversion"
+            enabled = false
             path = "gitversion"
             arguments = "/output buildserver /updateassemblyinfo true"
             formatStderrAsError = true
