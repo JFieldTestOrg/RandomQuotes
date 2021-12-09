bb12863116680c774db15be1b32231c856ff6908
dvinnai <dvinnai@TeamCity>
2021-12-09 09:49:05 +0000

    TeamCity change in 'DevOps / RandomQuotes' project: runners of 'CloudSmith Push Tag Test' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -247,7 +247,7 @@ object Test : BuildType({
             param("PackageNames", "Entities.Web.Files,Entities-Oneworld,AdminPortal")
             param("PackageVersions", "21.6.0-spe-469-0001,21.6.0-spe-469-0001,21.6.0-spe-469-0001")
             param("CloudsmithRepoName", "sre-dev-core")
-            param("PackageTag", "coconut")
+            param("PackageTag", "raspberry")
             param("CloudsmithOrganisation", "diligent")
             param("CloudsmithApiKey", "%env.CloudsmithApiKey%")
             param("PackageFormats", "nuget")
