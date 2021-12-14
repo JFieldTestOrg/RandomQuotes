e8817623416feae739332edc49f721ac49c58bfd
dvinnai <dvinnai@TeamCity>
2021-12-14 17:07:17 +0100

    TeamCity change in 'DevOps / RandomQuotes' project: parameters of 'CloudSmith Push Tag Test' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -234,8 +234,8 @@ object Test : BuildType({
 
     params {
         param("env.CloudsmithApiKey", "credentialsJSON:26421b48-39c2-428e-803b-6c179d454347")
-        param("GitVersion.FullSemVer", "%GitVersion.FullSemVer%")
         param("GitVersion.BranchName", "feature/OW-11927")
+        param("GitVersion.FullSemVer", "22.1.0-SPE-634.53")
     }
 
     vcs {
