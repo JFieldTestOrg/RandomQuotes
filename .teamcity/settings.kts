fa8d0ac5d4ad0f5ae7c99194fc1b3fabdb18c90a
dvinnai <dvinnai@TeamCity>
2022-01-06 15:55:20 +0000

    TeamCity change in 'DevOps / RandomQuotes' project: general settings of 'TestJob' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -231,6 +231,7 @@ object Push : BuildType({
 
 object Test : BuildType({
     name = "TestJob"
+    description = "Feel free to change this job if you need to test something"
 
     params {
         param("env.CloudsmithApiKey", "credentialsJSON:26421b48-39c2-428e-803b-6c179d454347")
